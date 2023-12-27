85102c483dfd87a94e0c2c65536c2781ec08fdf7
emredkyc <emredkyc@hotmail.com>
2023-12-27 22:16:36 +0100

    import dynamic incoming data to AnimeCard

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -4,13 +4,15 @@ import { fetchAnime } from "@/app/action";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { useInView } from "react-intersection-observer";
-import AnimeCard, { AnimeProp } from "./AnimeCard";
+import AnimeCard from "./AnimeCard";
 
 let page = 2;
 
+export type AnimeCard = JSX.Element;
+
 function LoadMore() {
   const { ref, inView } = useInView();
-  const [data, setData] = useState<AnimeProp[]>([]);
+  const [data, setData] = useState<AnimeCard[]>([]);
 
   useEffect(() => {
     if (inView) {
@@ -24,9 +26,7 @@ function LoadMore() {
   return (
     <>
       <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
-        {data.map((item: AnimeProp, index: number) => (
-          <AnimeCard key={item.id} anime={item} index={index} />
-        ))}
+        {data}
       </section>
       <section className="flex justify-center items-center w-full">
         <div ref={ref}>
