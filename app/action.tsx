d9dbf56e2b397b11f5fe98171ce0bd1d2f2c0f72
emredkyc <emredkyc@hotmail.com>
2023-12-27 22:04:40 +0100

    We need the tsx extension to return the presentation of the data

---
+++ app/action.tsx
@@ -0,0 +1,17 @@
+"use server";
+
+import AnimeCard, { AnimeProp } from "@/components/AnimeCard";
+
+const MAX_LIMIT = 8;
+
+export async function fetchAnime(page: number) {
+  const response = await fetch(
+    `https://shikimori.one/api/animes?page=${page}&limit=${MAX_LIMIT}&order=popularity`
+  );
+
+  const data = await response.json();
+
+  return data.map((item: AnimeProp, index: number) => (
+    <AnimeCard key={item.id} anime={item} index={index} />
+  ));
+}
